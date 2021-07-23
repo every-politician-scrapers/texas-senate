1586c655e58ac82269abc1de35fe3c03e89ef991
Tony Bowden <tony@tmtm.com>
2021-07-23 22:49:29 +0300

    Also fetch gender and age

--- bin/scraper/wikidata.rb
+++ bin/scraper/wikidata.rb
@@ -4,7 +4,8 @@
 require 'every_politician_scraper/wikidata_query'
 
 query = <<SPARQL
-  SELECT (STRAFTER(STR(?member), STR(wd:)) AS ?item) ?name
+  SELECT (STRAFTER(STR(?member), STR(wd:)) AS ?item) ?name ?gender
+     ?dob ?dobPrecision
      (STRAFTER(STR(?ps), '/statement/') AS ?psid)
   WHERE {
     ?member p:P39 ?ps .
@@ -14,6 +15,16 @@ query = <<SPARQL
     OPTIONAL { ?ps prov:wasDerivedFrom/pr:P1810 ?sourceName }
     OPTIONAL { ?member rdfs:label ?enLabel FILTER(LANG(?enLabel) = "en") }
     BIND(COALESCE(?sourceName, ?enLabel) AS ?name)
+
+    OPTIONAL { ?member wdt:P21 ?genderItem }
+    OPTIONAL {
+      ?member p:P569/psv:P569 [wikibase:timeValue ?dob ; wikibase:timePrecision ?dobPrecision]
+    }
+
+    SERVICE wikibase:label {
+      bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en".
+      ?genderItem rdfs:label ?gender
+    }
   }
   ORDER BY ?name
 SPARQL
