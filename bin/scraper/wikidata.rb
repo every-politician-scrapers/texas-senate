0faba5bbfc202f458885e0549e09ee89c7dde44b
Tony Bowden <tony@tmtm.com>
2021-07-24 14:51:43 +0300

    Include source information from Wikidata
    This way we can see if any statements are unsourced.

--- bin/scraper/wikidata.rb
+++ bin/scraper/wikidata.rb
@@ -4,15 +4,20 @@
 require 'every_politician_scraper/wikidata_query'
 
 query = <<SPARQL
-  SELECT (STRAFTER(STR(?member), STR(wd:)) AS ?item) ?name ?gender
-     ?dob ?dobPrecision
+  SELECT (STRAFTER(STR(?member), STR(wd:)) AS ?item)
+     ?name ?gender ?dob ?dobPrecision ?source
      (STRAFTER(STR(?ps), '/statement/') AS ?psid)
   WHERE {
     ?member p:P39 ?ps .
     ?ps ps:P39 wd:Q18565274 ; pq:P2937 wd:Q104767030 .
     FILTER NOT EXISTS { ?ps pq:P582 ?end }
 
-    OPTIONAL { ?ps prov:wasDerivedFrom/pr:P1810 ?sourceName }
+    OPTIONAL {
+      ?ps prov:wasDerivedFrom ?ref .
+      ?ref pr:P854 ?source .
+      FILTER CONTAINS(STR(?source), 'senate.texas.gov') .
+      OPTIONAL { ?ref pr:P1810 ?sourceName }
+    }
     OPTIONAL { ?member rdfs:label ?enLabel FILTER(LANG(?enLabel) = "en") }
     BIND(COALESCE(?sourceName, ?enLabel) AS ?name)
 
